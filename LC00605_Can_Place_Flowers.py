6185dc204d4a557d97df4b299ca448dc5738aeda
Bodong <bodong.zhang@yahoo.com>
2025-02-05 21:54:48 -0700

    Create LC00605_Can_Place_Flowers.py

---
+++ LC00605_Can_Place_Flowers.py
@@ -0,0 +1,54 @@
+# LC00605_Can_Place_Flowers.py
+
+# You have a long flowerbed in which some of the plots are planted, and some are not. 
+# However, flowers cannot be planted in adjacent plots.
+
+# Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 
+# means not empty, and an integer n, return true if n new flowers can be planted in 
+# the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: flowerbed = [1,0,0,0,1], n = 1
+# Output: true
+# Example 2:
+
+# Input: flowerbed = [1,0,0,0,1], n = 2
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= flowerbed.length <= 2 * 10**4
+# flowerbed[i] is 0 or 1.
+# There are no two adjacent flowers in flowerbed.
+# 0 <= n <= flowerbed.length
+
+from typing import List
+
+def canPlaceAFlower(flowerbed, i):
+    if (i==0 or flowerbed[i-1]==0) and (i==len(flowerbed)-1 or flowerbed[i+1]==0):
+        return True
+    else:
+        return False
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n==0:
+            return True
+
+        remaining=n
+
+        for i, value in enumerate(flowerbed):
+            if value==0 and canPlaceAFlower(flowerbed,i):
+                flowerbed[i]=1
+                remaining-=1
+                if remaining==0:
+                    return True
+                
+        return False
+        
+
